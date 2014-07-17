eb30b29138a0ef3899265554b87113c5f9750339
Juan Hoyos <juan.hoyosr@gmail.com>
2014-07-17 10:41:51 -0500

    Initial checkout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotelPal::Application.config.secret_key_base = '50ae49c3e5f56f332f3fe17eb5dd4d42fb8fa653e836bd17b1b222b64fc31dc39aaaefdd4d6aca73392a73680097ac28b08e2df5a2887aee9dfc6cad3892edf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HotelPal::Application.config.session_store :cookie_store, key: '_hotel-pal_session'
