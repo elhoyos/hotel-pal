2587b7bc412f1af31b459c0a1bbf7cd009f6cad7
Juan Hoyos <juan.hoyosr@gmail.com>
2014-07-18 12:18:34 -0500

    Sign up is now the default home page when not auth. Removed unused file.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ HotelPal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'users#show'
+  resources :users, only: :show
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
